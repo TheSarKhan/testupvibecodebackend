257653b4fb4cf71b526b9eaf1450cbfd79aed6da
TheSarKhan <serxan.babayev.06@gmail.com>
2026-03-13 01:05:22 +0400

    CORS

--- src/main/java/az/testup/config/CorsConfig.java
+++ src/main/java/az/testup/config/CorsConfig.java
@@ -14,7 +14,10 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "http://185.158.151.148:9091"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
